61e67d79f92e30de7f26a5ea7e450008a630e988
Mohamed Hisham <mohamed1991hisham@gmail.com>
2015-04-08 15:28:52 +0200

    Create StackQueue.h

---
+++ StackQueue.h
@@ -0,0 +1,23 @@
+	Stack * createStack(int stacksize);
+
+	int isEmptyS(Stack * stackname);
+
+	int pop(Stack * stackname);
+
+	void push(Stack * stackname,int Value);
+
+	int stackSize(Stack * stackname);
+
+	void deleteStack(Stack * stackname);
+
+	Queue * creatQueue(int QueueSize);
+
+	int dequeue(Queue * queueName);
+
+	void inqueue(Queue * queueName , int value);
+
+	int isEmptyQ(Queue * queueName);
+
+	int QueueSize(Queue * queueName);
+
+	void deleteQueue(Queue * queueName)
